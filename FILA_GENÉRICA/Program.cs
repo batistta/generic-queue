39ed3e20deca385885f4110422d1d15e43a96869
batistta <assuerio12@gmail.com>
2020-08-20 21:07:37 -0300

    initial commit

---
+++ FILA_GENÉRICA/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FILA_GENÉRICA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        class Celula<T>
+        {
+            public T dado { get; set; }
+            public Celula<T> prox { get; set; }
+        }
+        class FilaDinamica<T>
+        {
+            public Celula<T> Primeiro;
+            public Celula<T> Ultimo;
+            int tam;
+
+            public FilaDinamica()
+            {
+                Primeiro = new Celula<T>();
+                Ultimo = Primeiro;
+                Primeiro.prox = null;
+            }
+
+            public void Enfileirar(T dado)
+            {
+                Celula<T> temp = new Celula<T>();
+                temp.dado = dado;
+                temp.prox = null;
+
+                Ultimo.prox = temp;
+                Ultimo = temp;
+
+                tam++;
+            }
+            public bool Vazia()
+            {
+                return Primeiro == Ultimo;
+            }
+
+            public T Desenfileirar()
+            {
+                if (Vazia())
+                {
+                    object r = "";
+                    Console.WriteLine("FILA VAZIA!");
+                    T retorno = (T)r;
+                    return retorno;
+                }
+                Primeiro = Primeiro.prox;
+                tam--;
+                return Primeiro.dado;
+            }
+        }
+    }
+}
